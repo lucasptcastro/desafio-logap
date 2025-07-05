dd52b4e55ae1bd663c5e527a1471d0abe1597a0b
lucasptcastro <lucasptcastro@gmail.com>
2025-07-05 15:01:25 -0300

    feat: removed signUp component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { auth } from "@/lib/auth";
 
 import { LoginForm } from "./_components/login-form";
-import { SignUpForm } from "./_components/sign-up-form";
 
 export default async function AuthenticationPage() {
   const session = await auth.api.getSession({
@@ -18,19 +16,7 @@ export default async function AuthenticationPage() {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Tabs defaultValue="login" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="login">Login</TabsTrigger>
-          <TabsTrigger value="register">Criar conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="login">
-          <LoginForm />
-        </TabsContent>
-
-        <TabsContent value="register">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
+      <LoginForm />
     </div>
   );
 }
